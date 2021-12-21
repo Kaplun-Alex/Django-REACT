c840fc12f0908a2b45f2a4f78d3e8ee77ae0fafd
Alex <alexzeus@ukr.net>
2021-12-21 21:07:05 +0200

    Alive DEMO

--- djangoreactproject/frontend/src/App.js
+++ djangoreactproject/frontend/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom'
-import { Route} from 'react-router-dom'
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
 
 import  CustomersList from './CustomersList'
 import  CustomerCreateUpdate  from './CustomerCreateUpdate'
@@ -8,27 +7,29 @@ import './App.css';
 
 const BaseLayout = () => (
   <div className="container-fluid">
-<nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <a className="navbar-brand" href="#">Django React Demo</a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-  <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div className="navbar-nav">
-      <a className="nav-item nav-link" href="/">CUSTOMERS</a>
-      <a className="nav-item nav-link" href="/customer">CREATE CUSTOMER</a>
-
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="#">Django React Demo</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" 
+        data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" 
+        aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon">
+        </span>
+      </button>
+    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div className="navbar-nav">
+        <Link className="nav-item nav-link" to="/">CUSTOMERS</Link>
+        <Link className="nav-item nav-link" to="/customer">CREATE CUSTOMER</Link>
+      </div>
     </div>
-  </div>
-</nav>
+    </nav>
 
-    <div className="content">
-      <Route path="/" exact component={CustomersList} />
-      <Route path="/customer/:pk"  component={CustomerCreateUpdate} />
-      <Route path="/customer/" exact component={CustomerCreateUpdate} />
-
-    </div>
-
+      <div className="content">
+        <Routes>
+          <Route path="/" element={< CustomersList />} />
+          <Route path="/customer/:pk"  element={<CustomerCreateUpdate />} />
+          <Route path="/customer/" element={<CustomerCreateUpdate />} />
+        </Routes>
+      </div>
   </div>
 )
 
